9742ba9cc81dc880c6857119d6a62da572c3900f
Administrator <Administrator@gta-zhanghongchang>
2014-07-24 09:16:00 +0800

    java practice

--- git_demo/src/com/test/HelloWorld.java
+++ git_demo/src/com/test/HelloWorld.java
@@ -5,7 +5,19 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//System.out.println("Hello world");
+		int num = java.lang.Integer.MAX_VALUE;
+		short short_max = Short.MAX_VALUE;
+		byte btye_max = Byte.MAX_VALUE;
 		
+		System.out.println("num = " + num);
+		System.out.println("num = " + short_max);
+		System.out.println("num = " + btye_max);
+		
+		int array_1[];
+		array_1 = new int[10];
+		
+		System.out.println("length = " + array_1.length);
 		
 	}
 
