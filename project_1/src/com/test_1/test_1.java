6f0b8e1e864e9274364115480f1f3f9f37fbf029
Administrator <Administrator@gta-zhanghongchang>
2014-07-29 11:32:42 +0800

    进入任务实训选择页面

--- project_1/src/com/test_1/test_1.java
+++ project_1/src/com/test_1/test_1.java
@@ -24,18 +24,32 @@ public class test_1 {
 	public static void main(String[] args) {
 		//System.out.println("hello, test1");
 		
-		WebDriver IeWebDriver = new InternetExplorerDriver();
+		WebDriver Driver = new InternetExplorerDriver();
 		
 		//IeWebDriver.get("www.baidu.com");
-		IeWebDriver.get("http://localhost:8280/ILP/");
+		Driver.get("http://localhost:8280/ILP/");
 		
-		IeWebDriver.findElement(By.name("userInfo.userNo")).clear();
-		IeWebDriver.findElement(By.name("userInfo.userNo")).sendKeys("u0201");
+		//输入用户名
+		Driver.findElement(By.name("userInfo.userNo")).clear();
+		Driver.findElement(By.name("userInfo.userNo")).sendKeys("u0201");
 		
-		IeWebDriver.findElement(By.name("userInfo.password")).clear();
-		IeWebDriver.findElement(By.name("userInfo.password")).sendKeys("123");
+		//输入密码
+		Driver.findElement(By.name("userInfo.password")).clear();
+		Driver.findElement(By.name("userInfo.password")).sendKeys("123");
 		
 		//IeWebDriver.findElement(By.xpath("//img[@onclick='javascript:login()']")).click();
-		IeWebDriver.findElement(By.xpath("//img[@src='/ILP/images/logindl.gif']")).click();
+		Driver.findElement(By.xpath("//img[@src='/ILP/images/logindl.gif']")).click();
+		
+		//选择单人综合实验按钮
+		Driver.findElement(By.xpath("(//input[@name='testCaseType'])[2]")).click();
+		
+		//点击查询
+		Driver.findElement(By.xpath("//img[@src='images/search.gif']")).click();
+		
+		//选择实训任务
+		Driver.findElement(By.xpath("//div[@class='l-grid-row-cell-inner l-grid-row-cell-inner-fixedheight']")).click();
+		
+		//点击确定
+		Driver.findElement(By.xpath("//img[@src='images/ok.gif']")).click();
 	}
 }
